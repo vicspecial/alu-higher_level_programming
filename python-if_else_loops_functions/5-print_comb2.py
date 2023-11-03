b681f41139ba63b9253b983bc412d92b5b5f82fa
Victoria <v.godstime@alustudent.com>
2023-11-03 13:04:21 -0700

    comb2

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+for tens_digit in range(10):
+    for ones_digital in range(tens_digit + 1, 10):
+        print(
+                "{:d}{:d}".format(tens_digit, ones_digit),
+                end=", " if tens_digit < 8 else "\n"
+                )
