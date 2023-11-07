25f279a44302ae90ff799cdd6edd0dd852e2e171
Victoria <v.godstime@alustudent.com>
2023-11-07 02:09:34 -0800

    integer

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -6,23 +6,3 @@ def print_list_integer(my_list=[]):
         else:
             raise Exception("List contains non-integer values")
 
-# Testing the function
-try:
-    print_list_integer([1, 2, 3])
-except Exception as e:
-    print(e)
-
-try:
-    print_list_integer([1])
-except Exception as e:
-    print(e)
-
-try:
-    print_list_integer([])
-except Exception as e:
-    print(e)
-
-try:
-    print_list_integer([1, 2, "H", 9])
-except Exception as e:
-    print(e)
