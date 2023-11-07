324909318bdeb92fcd07011b72d3afbae612e9b8
Victoria <v.godstime@alustudent.com>
2023-11-07 02:51:06 -0800

    element

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -5,11 +5,4 @@ def element_at(my_list, idx):
     else:
         return my_list[idx]
 
-# Testing the function
-print(element_at([1, 2, 3], 1))  # Output: 2
-print(element_at([1, 2, 3], 0))  # Output: 1
-print(element_at([1, 2, 3], 2))  # Output: 3
-print(element_at([1, 2, 3], 3))  # Output: None
-print(element_at([1, 2, 3], -1))  # Output: None
-print(element_at([1], 0))  # Output: 1
-print(element_at([1], 1))  # Output: None
+
