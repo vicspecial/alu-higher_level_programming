724ca7046725d9cdc3af84ec8ea6a4a2f023c733
Victoria <v.godstime@alustudent.com>
2023-11-16 03:05:23 -0800

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
 class Square:
     """
     A class that defines a square.
     """
+
     def __init__(self, size):
-        self ._self = size
+        self.__size = size
+
 
