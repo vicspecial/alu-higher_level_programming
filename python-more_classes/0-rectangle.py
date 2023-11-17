6a10201fe2eb68c857bcb2e6452804cfbe1196ed
Victoria <v.godstime@alustudent.com>
2023-11-17 04:11:33 -0800

    rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    Class that defines a rectangle
+    """
+
+    def __init__(self):
+        """
+        Initializes an empty rectangle
+        """
+        pass
+
+
+
