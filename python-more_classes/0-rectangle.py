0f9e5e499fa451b5cafe5fc7d37a02a269812b43
Victoria <v.godstime@alustudent.com>
2023-11-17 04:12:42 -0800

    rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -11,4 +11,18 @@ class Rectangle:
         pass
 
 
+python
+Rectangle = __import__('0-rectangle').Rectangle
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
+
+
+When you run this code, it should output:
+
+
+<class '0-rectangle.Rectangle'>
+{}
+
 
