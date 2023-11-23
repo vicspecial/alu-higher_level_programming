3c1a16c03a5bc051647f9d7b2bec0c801c907ba3
Victoria <v.godstime@alustudent.com>
2023-11-22 23:38:35 -0800

    5 square

---
+++ python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""The Sqaure clasee"""
+
+
+class Square:
+    """ initialize the instances"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+
+        """calculating area"""
+
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """getter for retrieving"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, new_size):
+        """ changing the value"""
+        self.__size = new_size
+
+        if not isinstance(new_size, int):
+            raise TypeError("size must be an integer")
+        elif new_size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = new_size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
