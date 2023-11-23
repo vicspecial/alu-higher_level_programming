402d8382a88d73a02d285f44918ef2c71e8288a2
Victoria <v.godstime@alustudent.com>
2023-11-22 23:34:29 -0800

    3 square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,54 +1,18 @@
 #!/usr/bin/python3
+""" We will try creating a class Square"""
+
+
 class Square:
-    """
-    A class to represent a square.
+    """ Let's initialize the class below"""
 
-    ...
-
-    Attributes
-    ----------
-    size : int
-        size of the square
-
-    Methods
-    -------
-    area(self)
-        Returns the area of the square.
-    """
-
-    def init(self, size=0):
-        """
-        Constructs all the necessary attributes for the Square object.
-
-        Parameters
-        ----------
-        size : int, optional
-            The size of the square (default is 0)
-
-        Raises
-        ------
-        TypeError
-            If size is not an integer
-        ValueError
-            If size is less than 0
-        """
-
+    def __init__(self, size=0):
+        """checking if the type size is integer"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
-        """
-        Returns the area of the square.
-
-        Returns
-        -------
-        int
-            The area of the square (size * size)
-        """
-
-        return self.__size ** 2
-`
+        """ calculating the area of the the square"""
+        return (self.__size * self.__size)
