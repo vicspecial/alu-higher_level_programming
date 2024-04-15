c832fa707a56105c0eb7b823a4a919d9ada87008
Victoria <v.godstime@alustudent.com>
2024-04-15 01:56:11 -0700

    n

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
-for letter in range(ord('a'), ord('z')+1):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print(chr(letter), end='')
+for i in range(97, 123):
+    if i == 101 or i == 113:
+        continue
+    else:
+        print("{:c}".format(i), end="")
 
 
