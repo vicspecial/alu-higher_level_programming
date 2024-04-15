ddc7cae822cd40effd486a9e61c0886dadf50598
Victoria <v.godstime@alustudent.com>
2024-04-15 02:15:34 -0700

    m

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+"""
 def islower(c):
-    return 97 <= ord(C) <= 112
+    if c >= 'a' and c <= 'z':
+        return True
+    else:
+        return False
+This could have worked but not all cases will be caught
+"""
+
+
+def islower(c):
+    valueInt = ord(c)
+    if valueInt >= 97 and valueInt <= 122:
+        return True
+    else:
+        return False
